85584ce75f17d9c49ad869c611947b53164f31ba
AngeManto98 <koutikaangemarie@gmail.com>
2020-11-11 08:55:47 +0000

    Add upload avatart, event image and add user controller, model user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all.order(name: :ASC).page(params[:page]).per(20)
+    @users = User.where(["name LIKE ?", "%#{params[:name]}%"]) if params[:name]
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :avatar, AvatarUploader
+
+  validates :name , presence: true , uniqueness: true
+
+  def avatar_thumbnail
+    if avatar.present?
+      avatar.url
+    else
+      '/default-avatar.png'
+    end
+  end
+
+  
 end
