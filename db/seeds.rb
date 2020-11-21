0e3478e9dc84210e401cd4dd68b5e58c1eaf751d
AngeManto98 <koutikaangemarie@gmail.com>
2020-11-21 13:18:27 +0000

    Update, Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,17 @@
 
 #User.create([name: "admin", email: "admin@gmail.com", avatar: "image.png", password: "ange1998", password_confirmation: "ange1998", admin: true])
 
-10.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "000000"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
+# 20.times do |n|
+#   name = Faker::Name.unique.name
+#   email = Faker::Internet.email
+#   avatar = Faker::Avatar.image
+#   password = "ange1998"
+#   User.create!(name: name,
+#                email: email,
+#                avatar: avatar,
+#                password: password,
+#                password_confirmation: password
+#                )
+# end
+
+#Event.create([title: "Title 1", content:"Content 1", image: "image.png", date_start:"2020-11-24", date_end: "2020-11-29", organizer_id: 35])
