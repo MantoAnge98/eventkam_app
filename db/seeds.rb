48ad72b8c79662bdedc32c298edc3b19dc92a36c
AngeManto98 <koutikaangemarie@gmail.com>
2020-11-21 20:34:30 +0000

    update faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create([name: "admin", email: "admin@gmail.com", avatar: "image.png", password: "ange1998", password_confirmation: "ange1998", admin: true])
+#User.create([name: "admin", email: "admin@gmail.com", avatar: "image.png", password: "ange1998", password_confirmation: "ange1998", admin: true])
 
 # 20.times do |n|
 #     name = Faker::FunnyName.name
@@ -13,4 +13,25 @@ User.create([name: "admin", email: "admin@gmail.com", avatar: "image.png", passw
 #                 )
 # end
 
-#Event.create([title: "Title 1", content:"Content 1", image: "image.png", date_start:"2020-11-24", date_end: "2020-11-29", organizer_id: 35])
+
+Event.create([title: "Title 3", content:"Content 3", image: "image.png", date_start:"2020-12-24", date_end: "2020-12-29", organizer_id: 2])
+Event.create([title: "Title 4", content:"Content 4", image: "image.png", date_start:"2021-01-04", date_end: "2021-01-29", organizer_id: 3])
+Event.create([title: "Title 5", content:"Content 5", image: "image.png", date_start:"2021-01-04", date_end: "2021-01-29", organizer_id: 4])
+Event.create([title: "Title 6", content:"Content 6", image: "image.png", date_start:"2021-02-05", date_end: "2021-03-09", organizer_id: 5])
+Event.create([title: "Title 7", content:"Content 7", image: "image.png", date_start:"2021-02-05", date_end: "2021-03-09", organizer_id: 6])
+Event.create([title: "Title 8", content:"Content 8", image: "image.png", date_start:"2021-11-24", date_end: "2021-11-29", organizer_id: 7])
+Event.create([title: "Title 9", content:"Content 9", image: "image.png", date_start:"2021-11-24", date_end: "2021-11-29", organizer_id: 8])
+Event.create([title: "Title 10",content:"Content 10", image: "image.png", date_start:"2020-11-24", date_end: "2020-11-29", organizer_id: 9])
+
+Event.create([title: "Title 1", content:"Content 1", image: "image.png", date_start:"2020-11-24", date_end: "2020-11-29", organizer_id: 10])
+Event.create([title: "Title 2", content:"Content 2", image: "image.png", date_start:"2020-12-24", date_end: "2020-12-29", organizer_id: 11])
+Event.create([title: "Title 3", content:"Content 3", image: "image.png", date_start:"2020-12-24", date_end: "2020-12-29", organizer_id: 12])
+Event.create([title: "Title 4", content:"Content 4", image: "image.png", date_start:"2021-01-04", date_end: "2021-01-29", organizer_id: 13])
+Event.create([title: "Title 5", content:"Content 5", image: "image.png", date_start:"2021-01-04", date_end: "2021-01-29", organizer_id: 14])
+Event.create([title: "Title 6", content:"Content 6", image: "image.png", date_start:"2021-02-05", date_end: "2021-03-09", organizer_id: 15])
+Event.create([title: "Title 7", content:"Content 7", image: "image.png", date_start:"2021-02-05", date_end: "2021-03-09", organizer_id: 16])
+Event.create([title: "Title 8", content:"Content 8", image: "image.png", date_start:"2021-11-24", date_end: "2021-11-29", organizer_id: 17])
+Event.create([title: "Title 9", content:"Content 9", image: "image.png", date_start:"2021-11-24", date_end: "2021-11-29", organizer_id: 18])
+Event.create([title: "Title 10",content:"Content 10", image: "image.png", date_start:"2020-11-24", date_end: "2020-11-29", organizer_id: 19])
+Event.create([title: "Title 11", content:"Content 11", image: "image.png", date_start:"2020-11-30", date_end: "2020-12-09", organizer_id: 20])
+Event.create([title: "Title 12", content:"Content 12", image: "image.png", date_start:"2020-11-30", date_end: "2020-12-09", organizer_id: 20])
